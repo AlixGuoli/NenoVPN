14acd99082c616be0613d97f2faba1d616e95f85
AlixHu <1425300103@qq.com>
2025-09-19 16:04:35 +0800

    Initial Commit

---
+++ NeonVPN/NeonVPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeonVPNApp.swift
+//  NeonVPN
+//
+//  Created by Stephen Schaaf on 2025/9/19.
+//
+
+import SwiftUI
+
+@main
+struct NeonVPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
